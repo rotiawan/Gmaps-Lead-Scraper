9941bbd7edaac189c98632494f4e3bc40b00e21f
Muhammad Azfa Haqqoni <muhammadazfahaqqoni@gmail.com>
2025-10-16 14:19:06 +0700

    commit

--- gmaps_scraper/gmaps_scraper.py
+++ gmaps_scraper/gmaps_scraper.py
@@ -1,16 +1,3 @@
-import time
-from datetime import datetime
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-# GANTI FUNGSI LAMA KAMU DENGAN VERSI TERAKHIR YANG PALING AKURAT INI
-# Kita butuh library baru untuk 'membaca' URL
 from urllib.parse import urlparse
 
 import time
